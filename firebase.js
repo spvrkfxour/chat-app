9bd9e5a500f07c9ff8bf885711dd07a1dadab5b0
spvrkfxour <spvrkfxour@gmail.com>
2023-03-01 12:04:13 +0300

    installed navigation

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSgSOlo00cU9LhP595keftOHAxjIZOulo",
+    authDomain: "chat-app-5b309.firebaseapp.com",
+    projectId: "chat-app-5b309",
+    storageBucket: "chat-app-5b309.appspot.com",
+    messagingSenderId: "537167609840",
+    appId: "1:537167609840:web:0ab54679798f523494a735"
+  };
